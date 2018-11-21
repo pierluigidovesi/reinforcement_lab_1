e01480da69f04d5c077bb7666d03be20c37fc919
Pier Luigi Dovesi <32616813+pierluigidovesi@users.noreply.github.com>
2018-11-21 11:46:17 +0100

    debug env

--- game_env.py
+++ game_env.py
@@ -16,7 +16,6 @@ walls = [[[0, 1], [0, 2]],
 
 maze_max = [4, 5]
 
-
 #class maze
 class Env:
     # init maze
@@ -25,20 +24,25 @@ class Env:
 
     # possible_actions
     def get_actions(self, agent_pos):
+        assert agent_pos[0] < maze_max[0] and agent_pos[1] < maze_max[1]
         actions_array = np.zeros(5)
         move_north = [agent_pos, [agent_pos[0] - 1, agent_pos[1]]]
         move_south = [agent_pos, [agent_pos[0] + 1, agent_pos[1]]]
-        move_east = [agent_pos, [agent_pos[0], agent_pos[1]-1]]
-        move_weast = [agent_pos, [agent_pos[0], agent_pos[1]+1]]
+        move_east  = [agent_pos, [agent_pos[0], agent_pos[1] + 1]]
+        move_weast = [agent_pos, [agent_pos[0], agent_pos[1] - 1]]
 
         if agent_pos[0] > 0 and not (move_north in walls) and not (move_north[::-1] in walls):
             actions_array[0] = 1
+            #print("north")
         if agent_pos[0] < maze_max[0] and not (move_south in walls) and not (move_south[::-1] in walls):
             actions_array[2] = 1
+            #print("south")
         if agent_pos[1] > 0 and not (move_weast in walls) and not (move_weast[::-1] in walls):
-            actions_array[1] = 1
-        if agent_pos[1] < maze_max[1] and not (move_east in walls) and not (move_east[::-1] in walls):
             actions_array[3] = 1
+            #print("weast")
+        if agent_pos[1] < maze_max[1] and not (move_east in walls) and not (move_east[::-1] in walls):
+            actions_array[1] = 1
+            #print("east")
         actions_array[4] = 1
 
         return actions_array
@@ -48,8 +52,9 @@ class Env:
 
         actions_array = self.get_actions(agent_pos)
         new_states = []
-
-        for i in range(actions_array.size()):
+        print("qui")
+        for i in range(np.size(actions_array)):
+            print(i)
             if actions_array[i] == 1:
                 if i == 0:
                     new_agent_pos = [agent_pos[0] - 1, agent_pos[1]]
@@ -63,16 +68,17 @@ class Env:
                     new_agent_pos = agent_pos
 
                 new_mino_pos = [mino_pos[0], mino_pos[1]+1]
-                new_states.append(new_agent_pos, new_mino_pos)
+                new_states.append([new_agent_pos, new_mino_pos])
 
                 new_mino_pos = [mino_pos[0], mino_pos[1]-1]
-                new_states.append(new_agent_pos, new_mino_pos)
+                new_states.append([new_agent_pos, new_mino_pos])
 
                 new_mino_pos = [mino_pos[0]+1, mino_pos[1]]
-                new_states.append(new_agent_pos, new_mino_pos)
+                new_states.append([new_agent_pos, new_mino_pos])
 
                 new_mino_pos = [mino_pos[0]-1, mino_pos[1]]
-                new_states.append(new_agent_pos, new_mino_pos)
+                new_states.append([new_agent_pos, new_mino_pos])
+
         return new_states
 
     # plot maze of a state
---
+++ game_policy.py
@@ -0,0 +1,2 @@
+from game_env import Env
+
