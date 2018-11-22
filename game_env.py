914c3b944a2cb32e650d07f98dc09d80a0c33b07
Pier Luigi Dovesi <32616813+pierluigidovesi@users.noreply.github.com>
2018-11-22 14:54:26 +0100

    complete pipeline, bug in matrix dimension in line 130

--- game_env.py
+++ game_env.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+import copy
 
 walls = [[[0, 1], [0, 2]],
          [[1, 1], [1, 2]],
@@ -108,38 +110,39 @@ class EnvAndPolicy:
         return 1/len(new_states)
 
     def get_index(self, state, step):
-        index_state_time = state
-        index_state_time.append(step)
+        index_state_time = copy.deepcopy(state)
+        index_state_time.append([step])
         index_state_time = [item for sublist in index_state_time for item in sublist]
         return tuple(index_state_time)
 
     def fill_value_and_policy(self, state, step):
+        matrix_index = self.get_index(state, step)
         value = np.array(np.ones(5) * -np.inf)
         possible_actions = self.get_actions(state)
-        if self.reward(state) != 0:
-            v_star[self.get_index(state, step)] = self.reward(state)
 
-        for action in possible_actions:
-            value[action] = self.reward(state)
-            possible_states = self.get_states_given_action(state, action)
-            for next_state in possible_states:
-                value[action] += self.prob_given_action(possible_states) * v_star[self.get_index(next_state, step+1)]
-        v_star[self.get_index(state, step)] = np.max(value)
-        a_star[self.get_index(state, step)] = np.argmax(value)
+        if self.reward(state) != 0 or step == 14:
+            v_star[matrix_index] = self.reward(state)
+        else:
+            for action in possible_actions:
+                value[action] = self.reward(state)
+                possible_states = self.get_states_given_action(state, action)
+                for next_state in possible_states:
+                    next_state_index = self.get_index(next_state, step+1)
+                    print(next_state_index)
+                    value[action] += self.prob_given_action(possible_states) * v_star[next_state_index]
+            v_star[matrix_index] = np.max(value)
+            a_star[matrix_index] = np.argmax(value)
 
         return 0
 
     def main_loop(self, horizon=horizon):
-        for i in range(horizon, -1, -1):
+        for i in tqdm(range(horizon-1, -1, -1)):
             for x in range(maze_max[0]):
                 for y in range(maze_max[1]):
                     for z in range(maze_max[0]):
                         for h in range(maze_max[1]):
                             state = [[x, y], [z, h]]
                             self.fill_value_and_policy(state, i)
-
-
-
 
     # plot maze of a state
     def plot_state(self,state):
@@ -160,4 +163,11 @@ class EnvAndPolicy:
         plt.axes().invert_yaxis()  # invert the y-axis so the first row of data is at the top
         plt.show()
 
+def main():
+    new_run = EnvAndPolicy()
+    new_run.main_loop(15)
+    print("done")
 
+
+if __name__ == "__main__":
+    main()
